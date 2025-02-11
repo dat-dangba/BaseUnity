c9c629c6e1767fbfa92941348501831f67206419
dat-dangba <dat.dangba@tapbi.vn>
2025-02-11 16:23:46 +0700

    Update BaseEvent.cs

--- Runtime/_Event/BaseEvent/BaseEvent.cs
+++ Runtime/_Event/BaseEvent/BaseEvent.cs
@@ -9,13 +9,18 @@ public abstract class BaseEvent : BaseMonoBehaviour
     [SerializeField]
     private bool isEventHappening;
     [SerializeField]
+    private bool isInEvent;
+    [SerializeField]
     private double eventTime;
     [SerializeField]
     private double nextEventTime;
+    [SerializeField]
+    private int numberOfEvents;
 
     protected double EventTime { get => eventTime; private set => eventTime = value; }
     protected double NextEventTime { get => nextEventTime; private set => nextEventTime = value; }
     protected bool IsEventHappening { get => isEventHappening; private set => isEventHappening = value; }
+    public bool IsInEvent { get => isInEvent; private set => isInEvent = value; }
 
     public virtual void CheckEventStatus()
     {
@@ -23,6 +28,17 @@ public abstract class BaseEvent : BaseMonoBehaviour
         DateTime currentDateTime = TimeManager.Instance.DateTimeOffset.LocalDateTime;
         TimeSpan timeElapsed = currentDateTime - startDateTime;
 
+        numberOfEvents = (int)(timeElapsed.TotalHours / (EventDuration() + BreakTime()).TotalHours);
+        if ((GetLoop() != -1 && numberOfEvents >= GetLoop()) || timeElapsed.TotalHours < 0)
+        {
+            isInEvent = false;
+            isEventHappening = false;
+            eventTime = 0;
+            nextEventTime = 0;
+            return;
+        }
+
+        isInEvent = true;
         isEventHappening = (timeElapsed.TotalHours % (EventDuration() + BreakTime()).TotalHours) < EventDuration().TotalHours;
 
         DateTime eventStartTime = startDateTime + TimeSpan.FromHours(Math.Floor(timeElapsed.TotalHours / (EventDuration() + BreakTime()).TotalHours) * (EventDuration() + BreakTime()).TotalHours);
@@ -70,6 +86,15 @@ public abstract class BaseEvent : BaseMonoBehaviour
             yield return null;
         }
         CheckEventStatus();
+    }
+
+    /*
+     * -1 lặp lại liên tục
+     * 1,2,3,4.. sô vòng lặp của event > 0 
+     */
+    protected virtual int GetLoop()
+    {
+        return -1;
     }
 
     protected virtual DateTime GetTimeStart()
