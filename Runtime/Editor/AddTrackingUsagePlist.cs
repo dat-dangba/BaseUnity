0ee33faad46ecbd7e1ddc281fec7c4a7668400e3
dat-dangba <dat.dangba@tapbi.vn>
2025-09-30 15:30:03 +0700

    Update AddTrackingUsagePlist.cs

--- Runtime/Editor/AddTrackingUsagePlist.cs
+++ Runtime/Editor/AddTrackingUsagePlist.cs
@@ -1,4 +1,3 @@
-#if UNITY_IOS
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEditor.iOS.Xcode;
@@ -6,26 +5,27 @@ using System.IO;
 
 public static class AddTrackingUsagePlist
 {
+#if UNITY_IOS
     [PostProcessBuild]
     public static void OnPostProcessBuild(BuildTarget target, string pathToBuiltProject)
     {
         if (target != BuildTarget.iOS) return;
-
+    
         string plistPath = Path.Combine(pathToBuiltProject, "Info.plist");
         PlistDocument plist = new PlistDocument();
         plist.ReadFromFile(plistPath);
-
+    
         PlistElementDict rootDict = plist.root;
-
+    
         const string key = "NSUserTrackingUsageDescription";
         string value = "Ứng dụng sử dụng IDFA để cá nhân hóa quảng cáo và thống kê hiệu quả.";
-
+    
         if (!rootDict.values.ContainsKey(key)) // tránh overwrite nếu user đã tự set
         {
             rootDict.SetString(key, value);
         }
-
+    
         File.WriteAllText(plistPath, plist.WriteToString());
     }
-}
 #endif
+}
